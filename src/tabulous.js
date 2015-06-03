5c56c4ee5f1a4988e85a7f66df2618d29fa52088
Jayin Ton <tonjayin@gmail.com>
2015-06-04 00:05:00 +0800

    :factory: refactor

--- src/tabulous.js
+++ src/tabulous.js
@@ -4,19 +4,19 @@
  * Further changes, comments: @aaronlumsden
  * Licensed under the MIT license
  */
-;(function ( $, window, document, undefined ) {
+;
+(function($, window, document, undefined) {
 
     var pluginName = "tabulous",
         defaults = {
-            effect: 'scale'
+            effect: 'scale',
+            onSwitch: function() {}
         };
 
-       // $('<style>body { background-color: red; color: white; }</style>').appendTo('head');
-
-    function Plugin( element, options ) {
+    function Plugin(element, options) {
         this.element = element;
         this.$elem = $(this.element);
-        this.options = $.extend( {}, defaults, options );
+        this.options = $.extend({}, defaults, options);
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
@@ -25,91 +25,42 @@
     Plugin.prototype = {
 
         init: function() {
-
+            //link tab
             var links = this.$elem.find('a');
-            var firstchild = this.$elem.find('li:first-child').find('a');
-            var lastchild = this.$elem.find('li:last-child').after('<span class="tabulousclear"></span>');
 
-            if (this.options.effect == 'scale') {
-             tab_content = this.$elem.find('div').not(':first').not(':nth-child(1)').addClass('hidescale');
-            } else if (this.options.effect == 'slideLeft') {
-                 tab_content = this.$elem.find('div').not(':first').not(':nth-child(1)').addClass('hideleft');
-            } else if (this.options.effect == 'scaleUp') {
-                 tab_content = this.$elem.find('div').not(':first').not(':nth-child(1)').addClass('hidescaleup');
-            } else if (this.options.effect == 'flip') {
-                 tab_content = this.$elem.find('div').not(':first').not(':nth-child(1)').addClass('hideflip');
-            }
+            this.$elem.find('div').not(':first').not(':nth-child(1)').addClass('hide'+ this.options.effect);
+            //firstdiv,内容页的容器container
+            var $tabs_container = this.$elem.find('.tabs_container');
 
-            var firstdiv = this.$elem.find('#tabs_container');
-            var firstdivheight = firstdiv.find('div:first').height();
+            //Tab 默认第一个
+            this.$elem.find('li:first-child').find('a').addClass('tabulous_active');
 
-            var alldivs = this.$elem.find('div:first').find('div');
-
-            alldivs.css({'position': 'absolute','top':'40px'});
-
-            firstdiv.css('height',firstdivheight+'px');
-
-            firstchild.addClass('tabulous_active');
-
-            links.bind('click', {myOptions: this.options}, function(e) {
+            links.bind('click', {
+                options: this.options,
+                $elem: this.$elem
+            }, function(e) {
                 e.preventDefault();
 
-                var $options = e.data.myOptions;
-                var effect = $options.effect;
+                var $from_tab = e.data.$elem.find('.tabulous_active');
+                var $to_tab = $(this);
+                var $from_content = $tabs_container.find('div' + $from_tab.attr('href'));
+                var $to_content = $tabs_container.find('div' + $to_tab.attr('href'));
 
-                var mythis = $(this);
-                var thisform = mythis.parent().parent().parent();
-                var thislink = mythis.attr('href');
+                $from_content.removeClass('show' + e.data.options.effect).addClass('make_transist').addClass('hide' + e.data.options.effect);
+                $to_content.removeClass('hide' + e.data.options.effect).addClass('make_transist').addClass('show' + e.data.options.effect);
 
-
-                firstdiv.addClass('transition');
-
-                links.removeClass('tabulous_active');
-                mythis.addClass('tabulous_active');
-                thisdivwidth = thisform.find('div'+thislink).height();
-
-                if (effect == 'scale') {
-                    alldivs.removeClass('showscale').addClass('make_transist').addClass('hidescale');
-                    thisform.find('div'+thislink).addClass('make_transist').addClass('showscale');
-                } else if (effect == 'slideLeft') {
-                    alldivs.removeClass('showleft').addClass('make_transist').addClass('hideleft');
-                    thisform.find('div'+thislink).addClass('make_transist').addClass('showleft');
-                } else if (effect == 'scaleUp') {
-                    alldivs.removeClass('showscaleup').addClass('make_transist').addClass('hidescaleup');
-                    thisform.find('div'+thislink).addClass('make_transist').addClass('showscaleup');
-                } else if (effect == 'flip') {
-                    alldivs.removeClass('showflip').addClass('make_transist').addClass('hideflip');
-                    thisform.find('div'+thislink).addClass('make_transist').addClass('showflip');
-                }
-
-
-                firstdiv.css('height',thisdivwidth+'px');
-
-                
-
+                e.data.options.onSwitch($from_tab, $to_tab, $from_content, $to_content);
 
             });
-
-           
-
-
-         
-            
-        },
-
-        yourOtherFunction: function(el, options) {
-            // some logic
         }
     };
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            new Plugin( this, options );
+    $.fn[pluginName] = function(options) {
+        return this.each(function() {
+            new Plugin(this, options);
         });
     };
 
-})( jQuery, window, document );
-
-
+})(jQuery, window, document);
